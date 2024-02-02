22e20a7ab2fa2882d1d1d0f9c7995b485761f9ed
Eric <tunasmoothie@gmail.com>
2024-02-02 18:42:29 +0800

    added haps srcs and updated directory structure

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -4,22 +4,42 @@
 create_project sx_c2c_smf ./vivado/sx_c2c_smf -part xczu4eg-fbvb900-2-e
 
 # Add various sources to the project
-add_files -fileset constrs_1 ./srcs/constrs
-add_files ./srcs/rtl
+add_files -fileset constrs_1 ./srcs/smf/constrs
+add_files ./srcs/smf/rtl
 
 # Now import/copy the files into the project
 import_files -force
 
 # Create block diagram
-source ./srcs/bd/bd_smf.tcl
+source ./srcs/smf/bd/bd_smf.tcl
 
 # Generate wrapper file for block diagram
-# make_wrapper -files [get_files ./vivado/camlink_dma_4panel/camlink_dma_4panel.srcs/sources_1/bd/camlink_dma_4panel/camlink_dma_4panel.bd] -top
+make_wrapper -files [get_files ./vivado/sx_c2c_smf/sx_c2c_smf.srcs/sources_1/bd/sx_c2c_smf/sx_c2c_smf.bd] -top
 
 # Set wrapper as top
-# add_files -norecurse ./vivado/camlink_dma_4panel/camlink_dma_4panel.gen/sources_1/bd/camlink_dma_4panel/hdl/camlink_dma_4panel_wrapper.v
+add_files -norecurse ./vivado/sx_c2c_smf/sx_c2c_smf.gen/sources_1/bd/sx_c2c_smf/hdl/sx_c2c_smf_wrapper.v
 set_property top sx_c2c_smf_wrapper [current_fileset]
 
 
 
 ########## FOR HAPS-SX ##########
+
+# Create the project and directory structure
+create_project sx_c2c_haps ./vivado/sx_c2c_haps -part xcvu19p-fsva3824-2-e
+
+# Add various sources to the project
+add_files -fileset constrs_1 ./srcs/haps/constrs
+add_files ./srcs/haps/rtl
+
+# Now import/copy the files into the project
+import_files -force
+
+# Create block diagram
+source ./srcs/haps/bd/bd_smf.tcl
+
+# Generate wrapper file for block diagram
+make_wrapper -files [get_files ./vivado/sx_c2c_haps/sx_c2c_haps.srcs/sources_1/bd/sx_c2c_haps/sx_c2c_haps.bd] -top
+
+# Set wrapper as top
+add_files -norecurse ./vivado/sx_c2c_haps/sx_c2c_haps.gen/sources_1/bd/sx_c2c_haps/hdl/sx_c2c_haps_wrapper.v
+set_property top sx_c2c_haps_wrapper [current_fileset]
